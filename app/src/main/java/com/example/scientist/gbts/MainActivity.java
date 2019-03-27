a93259e5d4c4a0ece261a6c522c314982a7ed5fc
M <15137029@gift.edu.pk>
2019-03-27 18:15:00 +0500

    THis is second changing

--- app/src/main/java/com/example/scientist/gbts/MainActivity.java
+++ app/src/main/java/com/example/scientist/gbts/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.print("Bus Tracking System!");
+        System.out.print("Bus Tracking System! is our final year project.");
     }
 }
